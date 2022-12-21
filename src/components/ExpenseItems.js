cbcdba461dec9d5f7be0afefac1af30ff822441c
Harish-port <harish.sq@gmail.com>
2022-12-21 23:25:56 +0530

    tt

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -3,10 +3,6 @@ import React from 'react';
 export default function ExpenseItems() {
     return (
         <div>
-            <div>March 28th 2021s</div>
-            <div>
-                <h2>Title</h2>
-            </div>
         </div>
     )
 }
