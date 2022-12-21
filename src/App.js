00079bce10ed61a89cd201910615e270e79610be
Harish S <46044707+Harish-port@users.noreply.github.com>
2022-12-21 23:08:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItems from './components/ExpenseItems';
+
+function App() {
+  return (
+    <div>
+      <h2>Lets get started</h2>
+      <ExpenseItems/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ExpenseItems() {
+    return (
+        <div>
+            <div>March 28th 2021s</div>
+            <div>
+                <h2>Title</h2>
+            </div>
+        </div>
+    )
+}
