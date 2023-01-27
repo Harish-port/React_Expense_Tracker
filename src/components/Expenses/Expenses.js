6c673d1cfad8c9d44e2c901223e19f52d0a96eed
Harish-port <harish.sq@gmail.com>
2023-01-27 17:11:32 +0530

    Filter list based on selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Expenses.css';
 import ExpenseItems from './ExpenseItems';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpenseFilter';
 
 export default function Expenses(props) {
-   const [filteredYear,setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2020');
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
+    const filteredExpenses = props.items.filter(data => data.date.getFullYear().toString() === filteredYear);
+    console.log(filteredExpenses);
     return (
         <>
             <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter = {filterChangeHandler}/>
-                <ExpenseItems title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-                <ExpenseItems title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-                <ExpenseItems title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-                <ExpenseItems title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
-                <ExpenseItems title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-                <ExpenseItems title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-                <ExpenseItems title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-                <ExpenseItems title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} />
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {filteredExpenses.map(item =>
+                    <ExpenseItems key={item.id} title={item.title} amount={item.amount} date={item.date} />
+                )}
             </Card>
         </>
     )
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,19 +39,19 @@ export default function ExpenseForm(props) {
                     <label>
                         Title
                     </label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} required/>
                 </div>
                 <div className='new-expense__control'>
                     <label>
                         Amount
                     </label>
-                    <input type="number" value={enteredAmount} min='0' step='0.01' onChange={amountChangeHandler} />
+                    <input type="number" value={enteredAmount} min='0' step='0.01' onChange={amountChangeHandler} required />
                 </div>
                 <div className='new-expense__control'>
                     <label>
                         Date
                     </label>
-                    <input type="date" value={enteredDate} min='2012-1-1' max='2022-12-31' onChange={dateChangeHandler} />
+                    <input type="date" value={enteredDate} min='2012-1-1' max='2022-12-31' onChange={dateChangeHandler} required />
                 </div>
             </div> 
             <div className='new-expense__actions'>
